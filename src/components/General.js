3af6203b2b436aa69185ca2fd8b889575eacce89
tsmith <tsmith2077@gmail.com>
2023-01-03 18:26:05 -0700

    Form displays. General section works, need to fix the other two.

--- src/components/General.js
+++ src/components/General.js
@@ -2,66 +2,44 @@ import React, { Component } from 'react'
 import '../styles/General.css'
 
 class General extends Component {
-    state = {
-
-    }
-  render() {
-    return (
-        <div className='general'>
-            <h3>General Info</h3>
-            <div className='genInfoCont'>
-                {
-                this.props.general.formView ? (
-                <form>
-                    <label htmlFor="nameInput">Enter full name:</label>
-                    <input
-                        onChange={(event) => this.props.onInputChange(event, 'general', 'name')}
-                        value={this.state.name}
-                        name="name"
-                        type="text"
-                        id="nameInput"
-                        required
-                    />
-                    <label htmlFor="emailInput">Enter email:</label>
-                    <input
-                        onChange={(event) => this.props.onInputChange(event)}
-                        value={this.state.email}
-                        name="email"
-                        type="text"
-                        id="emailInput"
-                        required
-                    />
-                    <label htmlFor="phoneInput">Enter phone number:</label>
-                    <input
-                        onChange={(event) => this.props.onInputChange(event)}
-                        value={this.state.phone}
-                        name="phone"
-                        type="text"
-                        id="nameInput"
-                        required
-                    />
-                    <div className='btnCont'>
-                        <button onClick={(event) => this.props.onSubmitEditClick(event, this.state)}>Submit</button>
-                    </div>
-                </form>
-                ) : (
-                <div className='genInfo'>
-                    <div className='textCont'>
-                        <p>Name:  {this.props.general.name}</p>
-                        <p>Email:  {this.props.general.email}</p>
-                        <p>Phone:  {this.props.general.phone}</p>
-                    </div>
-                    <div className='btnCont'>
-                        <button onClick={(event) => this.props.onSubmitEditClick(event, this.props.general)}>Edit</button>
-                    </div>
+    render() {
+        return (
+            <div className='general'>
+                <h3>General Info</h3>
+                <div className='genInfoCont'>
+                    <form>
+                        <label htmlFor="nameInput">Enter full name:</label>
+                        <input
+                            onChange={(event) => this.props.onInputChange(event, 'general', 'name')}
+                            value={this.props.name}
+                            name="name"
+                            type="text"
+                            id="nameInput"
+                            required
+                        />
+                        <label htmlFor="emailInput">Enter email:</label>
+                        <input
+                            onChange={(event) => this.props.onInputChange(event, 'general', 'email')}
+                            value={this.props.email}
+                            name="email"
+                            type="text"
+                            id="emailInput"
+                            required
+                        />
+                        <label htmlFor="phoneInput">Enter phone number:</label>
+                        <input
+                            onChange={(event) => this.props.onInputChange(event, 'general', 'phone')}
+                            value={this.props.phone}
+                            name="phone"
+                            type="text"
+                            id="nameInput"
+                            required
+                        />
+                    </form>
                 </div>
-                )
-                }
-            </div>
-        </div>
-        
-    )
-  }
+            </div> 
+        )
+    }
 }
 
 export default General;
